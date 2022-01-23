2863637ae2e16ee56673a7abffccc6678ed47d79
sixnu123 <98258675+sixnu123@users.noreply.github.com>
2022-01-23 17:47:13 +0700

    Add files via upload

---
+++ beranda_client.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Data Pegawai Pengadilan Agama Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img1/logopakis1b.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>PENGADILAN AGAMA KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Data Pegawai Pengadilan Agama Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data dari pegawai, pengelolaan data keuangan, <br>
+                            dan juga pengelolaan laporan Kinerja serta laporan Keuangan pegawai.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data pegawai dan data keuangan .<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
